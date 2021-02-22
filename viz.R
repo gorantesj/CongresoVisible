1a83fbfc6ef0e13ad9905288c8c7ef9445b4efd0
Déborah <deborahandrea.m@hotmail.com>
2021-02-22 16:20:19 -0600

    charts

--- viz.R
+++ viz.R
@@ -8,11 +8,11 @@ library(highcharter)
 
 #Con más citaciones
 tibble(congresista = c("Yenica Acosta", "Laureano Augusto", "Iván Agudelo",
-                                    "Richard Aguilar", "Modesto Aguilera", "Luis Albán",
-                                    "José Amar", "Miguel Amín", "Fabio Leduc", "Esperanza Martínez",
+                       "Richard Aguilar", "Modesto Aguilera", "Luis Albán",
+                       "José Amar", "Miguel Amín", "Fabio Leduc", "Esperanza Martínez",
                        "Juan Pérez", "Verónica Castro", "Luis Miguel", "Jorge Zepeda", "Germán Hoyos",
                        "Mariana Hijar", "Anatolio Hernández", "Irma Herrera", "Norma Hurtado", "Germán Rojas"),
-                    n = c(sample(13:5, size = 20, replace = T)),
+       n = c(sample(13:5, size = 20, replace = T)),
        grupo = rep(c("Senado", "Representantes"), 10)) %>%
   # group_by(grupo) %>%
   arrange(desc(n)) %>%
@@ -83,21 +83,21 @@ tibble(edad = sample(30:78, size = 172, replace = T),
          n2= case_when(sexo == "Hombre"~ n*-1,
                        sexo == "Mujer"~ n)) %>%
   hchart(hcaes(y = n2, group = sexo, x = rango), type = "bar") %>%
-    hc_plotOptions(bar = list(stacking = T, borderRadius = 5,
-      dataLabels= list(enabled =F,
-                       align= "center",
-                       inside= F,
-                       # rotation = 5,
-                       verticalAlign= 'top',
-                       # x = -5,
-                       crop= F,
-                       overflow= "none",
-                       style=list(color="BLACK", fontSize = "25px", textOutline= "3px contrast"),
-                       format = paste0("{point.n:,.0f} ")
+  hc_plotOptions(bar = list(stacking = T, borderRadius = 5,
+                            dataLabels= list(enabled =F,
+                                             align= "center",
+                                             inside= F,
+                                             # rotation = 5,
+                                             verticalAlign= 'top',
+                                             # x = -5,
+                                             crop= F,
+                                             overflow= "none",
+                                             style=list(color="BLACK", fontSize = "25px", textOutline= "3px contrast"),
+                                             format = paste0("{point.n:,.0f} ")
 
-      )
-    ))%>%
-    hc_yAxis(labels= list(formatter = JS("function(){ return Math.abs(this.value); }")))
+                            )
+  ))%>%
+  hc_yAxis(labels= list(formatter = JS("function(){ return Math.abs(this.value); }")))
 
 #Profesion
 tibble(partido =c("AICO", "AICO",
@@ -126,7 +126,7 @@ n = c(1,0, 9, 9, 3,18,23, 28,17, 30, 19, 32,0,1, 3, 2,
       1,1, 3,1, 0,1, 0,1, 13,21, 5,4, 15,35, 1,2, 3,1,
       1,4, 14,25, 6,2 ),
 camara = c(rep(c("Senadores", "Representantes"),20 ))) %>%
-filter(camara == "Senadores") %>%
+  filter(camara == "Senadores") %>%
   # group_by(grupo) %>%
   arrange(desc(n)) %>%
   hchart(hcaes(label = partido, y = n, name = partido), type = "item") %>%
@@ -217,34 +217,164 @@ tot %>% mutate( year= floor_date(fecha, unit = "year")) %>%
 
 # Temas recurrentes
 tibble(fecha=seq(from = today()-23*365, to =today(), length.out = 8396 ),
-              tema= sample(c("Econimía", "Salud", "Educación",
-                           "Medio Ambiente", "Turismo"), size = 8396, replace = T)) %>%
+       tema= sample(c("Econimía", "Salud", "Educación",
+                      "Medio Ambiente", "Turismo"), size = 8396, replace = T)) %>%
   mutate( year= floor_date(fecha, unit = "year")) %>%
   count(year, tema) %>%
   mutate(year = datetime_to_timestamp(year)) %>%
   hchart(hcaes(x = year, y = n, group = tema), type = "line") %>%
   hc_xAxis(type = "datetime") %>%
   hc_yAxis(title = list(text = "Proyectos de ley"))
-  hc_tooltip(shared = T)
+hc_tooltip(shared = T)
 
 
 # Tiempo de aprobación
-  tibble(gob = c( "1998" ,"2001", "2004", "2007", "2010", "2013", "2016", "2019"),
-         )
+tibble(gob = c( "1998" ,"2001", "2004", "2007", "2010", "2013", "2016", "2019"),
+)
 
- aux<-  tibble(fecha=seq(from = today()-23*365, to =today(), length.out = 8396 ),
-         n = sample(c(0,1, 0,2, 0), size = 8396, replace = T))  %>%
-   mutate( year= floor_date(fecha, unit = "year"))
-  tibble(fecha=seq(from = today()-23*365, to =today(), length.out = 8396 ),
-         n = sample(c(0,1, 0,2, 0), size = 8396, replace = T))  %>%
-    mutate( year= floor_date(fecha, unit = "year")) %>%
-    # group_by(year) %>%  summarise(n = sum(n)) %>%
-    ggplot()+
-    geom_boxplot(aes(x = year, y =n, group = year))
+aux<-  tibble(fecha=seq(from = today()-23*365, to =today(), length.out = 8396 ),
+              n = sample(c(0,1, 0,2, 0), size = 8396, replace = T))  %>%
+  mutate( year= floor_date(fecha, unit = "year"))
+tibble(fecha=seq(from = today()-23*365, to =today(), length.out = 8396 ),
+       n = sample(c(0,1, 0,2, 0), size = 8396, replace = T))  %>%
+  mutate( year= floor_date(fecha, unit = "year")) %>%
+  # group_by(year) %>%  summarise(n = sum(n)) %>%
+  ggplot()+
+  geom_boxplot(aes(x = year, y =n, group = year))
 
 
-  hcboxplot(x = aux$year, var = aux$n)
+hcboxplot(x = aux$year, var = aux$n)
 data_to_boxplot(data = aux, variable = n, group_var = year)
 
 
 
+
+
+
+# Congresistas ------------------------------------------------------------
+# Proporción de mujeres
+tibble(hombres = sample(50:100, size = 24, replace = T),
+       year = c(1998:2021),
+       mujeres = 100-hombres) %>%  gather(sexo, n, c(hombres,mujeres )) %>%
+  hchart(hcaes(x = year, group= sexo, y =n), type = "area")
+
+#Edad promedio por año
+tibble( year = c(1998:2021),
+        media = sample(40:87, size = 24, replace = T),
+        q1 =round(quantile(media, probs = .25),2),
+        q3 =round(quantile(media, probs = .75),2)) %>%
+  mutate(year = as.character(year)) %>%
+  ggplot +
+  geom_bar( aes(x=fct_reorder(year,media) , y=media), stat="identity", fill="#720026", alpha=0.8, width=0.5) +
+  geom_errorbar( aes(x=year, ymin=q1, ymax=q3), width=0.2, colour="black", alpha=0.9, size=.5) +
+  #titulos
+  labs( y ="Promedio", x = "")+
+  # temas
+  theme_minimal()+
+  theme(legend.position="none",
+        panel.grid.minor = element_blank())+
+  coord_flip()
+
+#Top partidos
+tibble(year = c(1998:2021),
+       partido1 =sample(x = c(.1,.3, .4, .2), size = 24, replace = T),
+       partido2 =sample(x = c(.1,.3, .2), size = 24, replace = T),
+       partido3 =sample(x = c(.1,.3, .2), size = 24, replace = T)) %>%
+  gather(grupo, n, partido1:partido3) %>%
+  hchart(hcaes(x = year, group = grupo, y =n), type = "column") %>%
+  hc_plotOptions(column = list(stacking = T))
+
+# Periodos de mujeres en el cargo
+
+tibble(min = c(1998, 1998, 2006, 2002, 2006, 2006, 2010, 2010, 2014, 2014),
+       max = c(2002, 2006, 2010, 2006, 2018, 2010, 2014, 2014, 2018, 2021),
+       congresista = c( "Ana H.", "Fabiola V.", "Lucía M.", "Sara H.", "Nerea J.",
+                       "Lusa D.", "Andrea E.", "Julia Z.", "Mariana R.", "Fenanda T.")) %>%
+  arrange(min) %>%
+  hchart(hcaes(x = congresista, low = min, high = max), type = "columnrange") %>%
+  hc_plotOptions(columnrange = list(borderRadius = 8,pointWidth = 10)) %>%
+  hc_chart(inverted = T)
+
+# % Iniciativas por ministro y aprobadas
+
+tibble(year =c(1998:2021),
+       min = sample(x = c(5:57), size = 24, replace = T)) %>%
+  hchart(hcaes(x = year,  y = min), type = "area")
+
+
+# % Iniciativas aprobadas de ministros
+
+tibble(year =c(1998:2021),
+       min = sample(x = c(80:92), size = 24, replace = T)) %>%
+  hchart(hcaes(x = year,  y = min), type = "area")
+
+
+# Votación ----------------------------------------------------------------
+
+# Días que se tardan en aprobar dependiendo el partido que propuso el proyecto
+aux <- tibble(partido =sample(c("CD", "CR", "AV","PDA", "PLC",
+                "CD", "CR", "PSUN", "PCC", "Decentes"), size = 100, replace = T),
+       dias = sample(10:87, size = 100, replace = T)) %>%
+  group_by(partido) %>% summarise(min= min(dias), max= max(dias),
+                                  mean = mean(dias)) %>%
+  arrange(mean)
+aux %>%
+hchart(hcaes(x = partido, low = min, high = max), type = "columnrange") %>%
+  hc_add_series(aux, hcaes(x = partido, y = mean), type = "scatter", color = "#FFF") %>%
+  hc_plotOptions(columnrange = list(borderRadius = 8,pointWidth = 10)) %>%
+  hc_chart(inverted = T)
+
+# votación por tema
+
+tibble(votos = sample(30:78, size = 172, replace = T),
+       voto = sample(c("En contra", "A favor"), size = 172, replace = T, prob = c(.5, .5))) %>%
+  mutate(tema = cut(votos,c(17,29,39,49,59,69,79, 89,100),
+                     labels = c("Economía","Salud","Educación", "Infraestructura",
+                                "Seguridad", "Turismo","Medio ambiente", "Hacienda"))) %>%
+  count(tema, voto) %>%
+  mutate(n = as.double(n),
+         n2= case_when(voto == "En contra"~ n*-1,
+                       voto == "A favor"~ n)) %>%
+  hchart(hcaes(y = n2, group = voto, x = tema), type = "bar") %>%
+  hc_plotOptions(bar = list(stacking = T, borderRadius = 5,
+                            dataLabels= list(enabled =F,
+                                             align= "center",
+                                             inside= F,
+                                             # rotation = 5,
+                                             verticalAlign= 'top',
+                                             # x = -5,
+                                             crop= F,
+                                             overflow= "none",
+                                             style=list(color="BLACK", fontSize = "25px", textOutline= "3px contrast"),
+                                             format = paste0("{point.n:,.0f} ")
+
+                            )
+  ))%>%
+  hc_yAxis(labels= list(formatter = JS("function(){ return Math.abs(this.value); }")))
+
+# votación por partido
+
+tibble(votos = sample(30:78, size = 172, replace = T),
+       voto = sample(c("En contra", "A favor"), size = 172, replace = T, prob = c(.5, .5))) %>%
+  mutate(tema = cut(votos,c(17,29,39,49,59,69,79, 89,100),
+                    labels = c("CD", "CR", "AV","PDA", "PLC", "PSUN", "PCC", "Decentes"))) %>%
+  count(tema, voto) %>%
+  mutate(n = as.double(n),
+         n2= case_when(voto == "En contra"~ n*-1,
+                       voto == "A favor"~ n)) %>%
+  hchart(hcaes(y = n2, group = voto, x = tema), type = "bar") %>%
+  hc_plotOptions(bar = list(stacking = T, borderRadius = 5,
+                            dataLabels= list(enabled =F,
+                                             align= "center",
+                                             inside= F,
+                                             # rotation = 5,
+                                             verticalAlign= 'top',
+                                             # x = -5,
+                                             crop= F,
+                                             overflow= "none",
+                                             style=list(color="BLACK", fontSize = "25px", textOutline= "3px contrast"),
+                                             format = paste0("{point.n:,.0f} ")
+
+                            )
+  ))%>%
+  hc_yAxis(labels= list(formatter = JS("function(){ return Math.abs(this.value); }")))
