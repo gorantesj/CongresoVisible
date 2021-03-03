4fe1295d75845cc68ce25ff148bef1d7fdd9f3a6
hectorchavezgaro44 <hector.chaga94@gmail.com>
2021-03-03 13:43:13 -0600

    queries ya con graficas

--- hector_queries.R
+++ hector_queries.R
@@ -1,10 +1,10 @@
 require(pacman)
-p_load(tidyverse, RPostgreSQL, DBI, RMySQL,  RMariaDB)
+p_load(tidyverse,highcharter, RPostgreSQL, DBI, RMySQL,  RMariaDB, treemapify)
 
 
 con <- dbConnect(
   drv = RMariaDB::MariaDB(),
-  dbname = "prueba33_congresovisible",
+  dbname = "prueba33_congresovisible2",
   host = "162.241.62.202",
   username = "prueba33_cv_user",
   password = "CongresoVisible2020",
@@ -14,6 +14,11 @@ con <- dbConnect(
 dbListTables(con)
 tbl(con, "proyecto_leys")
 
+#Congreso hoy -> proyectos de ley -----------
+colores <- c("#1d67ac",  "#ff4f4f", "#93c73d", "#6d4fa0",
+             "#6b4c2a", "#f9a819", "#019188", "#d34787",
+             "#164a80", "#F05647", "#0e4c42", "#3b005a",
+             "#AD8156", "#fdc10e", "#00b140", "#e995bf" )
 # Queries estatus del proyecto por fecha
 tbl(con, "proyecto_leys") %>%
   count(fecha_radicacion, estado_proyecto_ley_id,camara_id, cuatrienio_id) %>%
@@ -27,9 +32,38 @@ tbl(con, "proyecto_leys") %>%
     camara == "Cámara de Representantes",
     cuatrienio_id == 1
   ) %>%
-  arrange(estado_proyecto_ley_id, fecha) %>%
-  show_query()
-
+  arrange(estado_proyecto_ley_id, fecha_radicacion) %>%
+  count(fecha_radicacion, estado_proyecto_ley_id) %>%
+  show_query() %>%
+  collect() %>%
+  hchart(hcaes(x = fecha_radicacion, y = n, group = estado_proyecto_ley_id), type = "line") %>%
+  hc_plotOptions(line= list(lineWidth = 4,
+                            marker = list(radius =0),
+                            stickyTracking=F)) %>%
+  hc_xAxis(crosshair = T, title = list(text = "Fecha",  style = list(color = "#FFF")), type = "datetime",
+           lineWidth = 0, tickWidth  = 0, gridLineWidth =0,
+           showLastLabel= F,
+           labels = list(step = 3, style = list(fontSize = "16px", color = "#FFF") )) %>%
+  hc_yAxis(crosshair = F, title = list(text = "Total de proyectos", style = list(color = "#FFF")), tickAmount = 3, max = 6, min =0,
+           dashStyle = "dot",
+           gridLineWidth =.5, showFirstLabel = F, gridLineColor = "",
+           labels = list( style = list(fontSize = "12px") )) %>%
+  #title
+  hc_title(text = "<b>Proyectos de ley",  style = list(color = "#FFF")) %>%
+  #tooltip
+  hc_tooltip(
+    borderWidth= 0,
+    outside = T,
+    textOutline= "3px contrast",
+    shadow=F,
+    shared = T,
+    split = F,
+    headerFormat= '<span style="font-size: 10px">{point.key}</span><br/>'
+    # pointFormat = '<span style="color:{point.color}">●</span> <b> {point.candidato}<b><br> p. clave: {point.palabra}<br> {point.n} tuits <br> {point.rt} retuits<br> {point.favs} favoritos<br>'
+  ) %>%
+  hc_colors(colors = c("#2C6170", "#FF6B6B", "#FFE66D")) %>%
+  hc_chart(style = list(fontFamily = "Avenir next"
+  ))
 
 # Queries de proyectos de ley presentados por ministros
 
@@ -61,11 +95,21 @@ tbl(con,"proyecto_ley_autors") %>%
     cuatrienio_id == 1
   )%>%
   count(congresista_id) %>%
-  show_query()
-
-
+  show_query() %>%
+  collect() %>%
+  ggplot(aes(x = fct_reorder(congresista_id, n),y =n, label = n, fill = congresista_id)) +
+  geom_chicklet(width = 0.55 )+
+  scale_fill_manual(values = colores)+
+  labs(x = "", y = "", title = "Proyectos presentados \npor ministros")+
+  geom_text(nudge_y = -1, size =6, color = "white")+
+  theme_minimal() +coord_flip()+
+  theme(legend.position = "none",
+        panel.grid = element_blank(),
+        axis.text.x = element_text(),
+        text=element_text(size=18, family="Avenir Next"))
 
 # Queries de tipo de proyecto
+pal <- colorRampPalette(c("#2B6170", "#93ACAF"))
 
 tbl(con,"proyecto_leys") %>%
   count(tipo_proyecto_id, camara_id, cuatrienio_id) %>%
@@ -75,20 +119,33 @@ tbl(con,"proyecto_leys") %>%
       select(id, camara = nombre),
     by = c("camara_id" = "id")
   )  %>%
+  left_join(
+    tbl(con, "tipo_proyectos") %>%
+      #filter(activo == 1) %>%
+      select(id, tipo_proyecto = nombre),
+    by = c("tipo_proyecto_id" = "id")
+  )  %>%
   filter( # filtro, puede cambiar
     camara == "Cámara de Representantes",
     cuatrienio_id == 1
   ) %>%
-  arrange(estado_proyecto_ley_id, fecha) %>%
-  show_query()
-
-
-
-
+  count(tipo_proyecto) %>%
+  show_query() %>%
+  collect() %>%
+  hchart(hcaes(x = tipo_proyecto,  value = n), type = "treemap") %>%
+  hc_plotOptions(treemap = list(borderRadius = 7, colorByPoint = T,
+    dataLabels = list(style = list(fontFamily = "Avenir next",
+                                   fontSize = "16px")))) %>%
+  hc_colors(colors = pal(11)) %>%
+  hc_chart(style = list(fontFamily = "Avenir next"
+  ))
 
 
 
 # Queries de proyectos radicados según sexo
+# Gráfica nueva
+# Pendiente por discutir con Colombia
+#La pongo en ggplot
 
 tbl(con,"proyecto_ley_autors") %>%
   count(proyecto_ley_id, congresista_id) %>%
@@ -108,15 +165,25 @@ tbl(con,"proyecto_ley_autors") %>%
       select(id, genero_id),
     by = c("congresista_id" = "id")
   ) %>%
+  left_join(
+    tbl(con, "generos") %>%
+      select(id, nombre),
+    by = c("genero_id" = "id")
+  ) %>%
   filter( # filtro, puede cambiar
     camara == "Cámara de Representantes",
     cuatrienio_id == 1
-  )%>%
+  ) %>%
+  count(tema_proyecto_ley_id, nombre) %>%
   show_query()
 
 
+ggplot(aes(area = tema_proyecto_ley_id, fill = n, label = tema_proyecto_ley_id,
+                subgroup = nombre)) +
+  geom_treemap() +
+  geom_treemap_subgroup_border()
 
-## Query proporción de mujeres---------------
+## Query historico -> composición -> proporción de mujeres---------------
 tbl(con, "congresistas") %>%
   select(cuatrienio_id , genero_id, es_representante_camara, es_senador) %>%
   left_join(tbl(con, "cuatrienios") %>%
@@ -126,12 +193,14 @@ tbl(con, "congresistas") %>%
             select(id, nombre),
             by=c("genero_id"="id")) %>%
   filter(es_representante_camara==1) %>%
-  group_by(cuatrienio_id,fecha_inicio, genero_id) %>%
+  group_by(cuatrienio_id,fecha_inicio, nombre) %>%
   summarise(total=n()) %>%
   mutate(totales=sum(total, na.rm=T),
          porc=round((total/totales)*100, 2)) %>%
-  show_query()
-
+  show_query() %>%
+  collect() %>%
+  hchart(hcaes(x = fecha_inicio, group= nombre, y =porc), type = "area") %>%
+  hc_plotOptions(area = list(stacking = T))
 
 #Query mediana de edad
 
@@ -146,8 +215,21 @@ tbl(con, "congresistas") %>%
   filter(es_representante_camara==1) %>%
   group_by(fecha_inicio) %>%
   summarise(median_edad=median(edad, na.rm=T))%>%
-    show_query()
-
+    show_query() %>%
+    collect() %>%
+    ggplot +
+    geom_bar( aes(x=fct_reorder(fecha_inicio,median_edad) , y=median_edad),
+              stat="identity",
+              fill="#720026", alpha=0.8, width=0.5) +
+    geom_errorbar( aes(x=fecha_inicio, ymin=q1, ymax=q3), width=0.2,
+                   colour="black", alpha=0.9, size=.5) +
+    #titulos
+    labs( y ="Mediana", x = "", title = "Edad mediana por año")+
+    # temas
+    theme_minimal()+
+    theme(legend.position="none",
+          panel.grid.minor = element_blank())+
+    coord_flip()
 
   ## Query partidos con más representación---------------
 
@@ -163,6 +245,9 @@ tbl(con, "congresistas") %>%
     group_by(cuatrienio_id, fecha_inicio, nombre) %>%
     summarise(total=n())%>%
     show_query()
+
+  hchart(hcaes(x = fecha_inicio, group = nombre, y =total), type = "column") %>%
+    hc_plotOptions(column = list(stacking = T))
 
   ## Query total de citaciones---------------
 
