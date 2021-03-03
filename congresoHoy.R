54b70ef4aef2110fd48d47be70dcf9ad507d3d63
Déborah <deborahandrea.m@hotmail.com>
2021-03-03 11:42:35 -0600

    proyectos de ley por partido

--- congresoHoy.R
+++ congresoHoy.R
@@ -22,3 +22,37 @@ tbl(con, "proyecto_leys") %>%
   show_query()
 
 #Proyectos de ley -> Número de PL en en trámite -> Proyectos en Senado
+
+
+#Proyectos de ley -> Actividad por Partido Políticos -> Total de autorías
+tbl(con, "proyecto_ley_autors") %>% filter(activo == 1) %>%  left_join(
+  tbl(con, "congresistas") %>% select(id, corporacion_id, partido_id) %>%
+    left_join(
+      tbl(con, "partidos") %>% filter(activo == 1) %>% select(id, partido = nombre, grupo = posicion_ideologica),
+      by = c("partido_id" = "id")
+    ) %>%
+    left_join(
+      tbl(con, "corporacions") %>% filter(activo == 1) %>% select(id, camara = nombre),
+      by = c("corporacion_id" = "id")
+    ) %>% select(id, partido, grupo, camara) ,
+  by = c("congresista_id"="id")
+) %>% count(partido) %>%  show_query()
+
+#Gráfica
+tbl(con, "proyecto_ley_autors") %>% filter(activo == 1) %>% count(congresista_id) %>% left_join(
+  tbl(con, "congresistas") %>% select(id, corporacion_id, partido_id) %>%
+    left_join(
+      tbl(con, "partidos") %>% filter(activo == 1) %>% select(id, partido = nombre, grupo = posicion_ideologica),
+      by = c("partido_id" = "id")
+    ) %>%
+    left_join(
+      tbl(con, "corporacions") %>% filter(activo == 1) %>% select(id, camara = nombre),
+      by = c("corporacion_id" = "id")
+    ) %>% select(id, partido, grupo, camara) ,
+  by = c("congresista_id"="id")
+) %>%
+  arrange(desc(n)) %>% collect() %>%
+  hchart(hcaes(x = partido, y = n, group = camara), type = "bar") %>%
+  hc_title(text = "Partidos con más autorías")
+
+
