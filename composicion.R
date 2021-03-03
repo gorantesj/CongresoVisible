0867734217472ef81478d54b5a5bc5083d07dff8
Jaziel Flores <jazzzfm@protonmail.com>
2021-03-03 17:46:27 -0600

    composicion

--- composicion.R
+++ composicion.R
@@ -44,19 +44,62 @@ tbl(con, "congresistas") %>% filter(activo == 1, cuatrienio_id == 1) %>%
 # Congresistas -> Pirámide Poblacional de Edad y sexo -> Por partido
 
 # Congresistas -> Cuatrienios en el Congreso -> Cuatrienios como Representantes
+tbl(con, "congresistas") %>% filter(activo == 1,  es_representante_camara == 1) %>%
+  select(congresista = nombre, cuatrienio_id) %>%
+  left_join(tbl(con, "cuatrienios") %>% select(cuatrienio_id = id, cuatrienio = nombre), by = "cuatrienio_id") %>%
+  select(congresista, cuatrienio) %>% show_query()
 
 # Congresistas -> Cuatrienios en el Congreso -> Cuatrienios como Senadores
+tbl(con, "congresistas") %>% filter(activo == 1,  es_senador == 1) %>%
+  select(congresista = nombre, cuatrienio_id) %>%
+  left_join(tbl(con, "cuatrienios") %>% select(cuatrienio_id = id, cuatrienio = nombre), by = "cuatrienio_id") %>%
+  select(congresista, cuatrienio) %>% show_query()
 
 # Congresistas -> Georepresentación
+tbl(con, "congresistas") %>% filter(activo == 1) %>%
+  select(congresista_id = id, congresista = nombre) %>%
+  left_join(
+    tbl(con, "res_campanias") %>%
+      select(congresista_id, departamento_id), by = "congresista_id") %>%
+  left_join(
+    tbl(con, "departamentos") %>% filter(activo == 1) %>%
+      select(departamento_id = id, departamento = nombre),
+    by = "departamento_id") %>%
+  show_query()
 
 # Congresistas -> Investigaciones
 # "jalar" 3 tipos de investigaciones: silla vacía, renunció  a la curul y los que empiecen por Atención:
 # pérdida de investidura.
 
 # Congresistas -> Cantidad de PL presentados por sexo
+tbl(con, "proyecto_ley_autors") %>%
+  select(proyecto_ley_autors_id = id, proyecto_ley_id, congresista_id) %>%
+  left_join(
+    tbl(con, "proyecto_leys") %>% select(proyecto_ley_id = id, proyecto = titulo),
+    by = "proyecto_ley_id") %>%
+  left_join(
+    tbl(con, "congresistas") %>% filter(activo == 1) %>%
+      select(congresista_id = id, congresista = nombre, genero_id),
+    by = "congresista_id") %>%
+  left_join(
+    tbl(con,"generos") %>% select(genero_id = id, genero = nombre),
+    by = "genero_id") %>%
+  count(genero) %>% show_query()
 
 # Congresistas -> Partidos Políticos
+tbl(con, "congresistas") %>% filter(activo == 1) %>%
+  select(congresista_id = id, congresista = nombre, partido_id) %>%
+  left_join(
+    tbl(con, "partidos") %>% filter(activo == 1) %>%
+      select(partido_id = id, partido = nombre),
+    by = "partido_id") %>% show_query()
 
 # Congresistas -> Circunscripción
+tbl(con, "congresistas") %>% filter(activo == 1) %>%
+  select(congresista_id = id, congresista = nombre, circunscripcion_id) %>%
+  left_join(
+    tbl(con, "circunscripcions") %>% filter(activo == 1) %>%
+      select(circunscripcion_id = id, circunscripcion = nombre),
+    by = "circunscripcion_id") %>% show_query()
 
 # Congresistas -> Minoría
