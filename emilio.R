f50e919f9c77f7014c295010e1a5820d4f46dded
EAMI91 <emiliomorones@gmail.com>
2021-03-03 11:37:24 -0600

    faltaban show_query

--- emilio.R
+++ emilio.R
@@ -4,7 +4,7 @@ library(RMySQL)
 library(highcharter)
 con <- dbConnect(
   drv = RMariaDB::MariaDB(),
-  dbname = "prueba33_congresovisible",
+  dbname = "prueba33_congresovisible2",
   host = "162.241.62.202",
   username = "prueba33_cv_user",
   password = "CongresoVisible2020",
@@ -230,7 +230,8 @@ tbl(con, "congresistas") %>% filter(activo == 1, cuatrienio_id == 1) %>%
   # left_join(tbl(con,"cuatrienios") %>% select(id,cuatrienio = nombre)) %>%
   mutate(años = round(DATEDIFF(CURDATE(),fechaNacimiento)/365)) %>%
   count(partido_id,genero_id,años) %>% left_join(tbl(con,"generos") %>% select(id,genero = nombre), by = c("genero_id" = "id")) %>%
-  left_join(tbl(con,"partidos") %>% select(id,partido = nombre), by = c("partido_id" = "id")) %>% ungroup %>% select(genero,años,partido,n)
+  left_join(tbl(con,"partidos") %>% select(id,partido = nombre), by = c("partido_id" = "id")) %>% ungroup %>%
+  select(genero,años,partido,n) %>% show_query()
 
 ## Congresistas
 ### Sexo
@@ -239,4 +240,4 @@ tbl(con, "congresistas") %>% filter(activo == 1, cuatrienio_id == 1) %>%
   # left_join(tbl(con,"cuatrienios") %>% select(id,cuatrienio = nombre)) %>%
   mutate(años = round(DATEDIFF(CURDATE(),fechaNacimiento)/365)) %>%
   count(años,genero_id) %>% left_join(tbl(con,"generos") %>% select(id,genero = nombre), by = c("genero_id" = "id")) %>%
-  ungroup %>% select(genero,años,n)
+  ungroup %>% select(genero,años,n) %>% show_query()
