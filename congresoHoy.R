d5c90f3bac10922adc71443a78528228b8c9dbb3
Déborah <deborahandrea.m@hotmail.com>
2021-03-03 11:11:11 -0600

    Módulo congreso hoy

---
+++ congresoHoy.R
@@ -0,0 +1,15 @@
+library(DBI)
+library(tidyverse)
+library(RMySQL)
+
+con <- dbConnect(
+  drv = RMariaDB::MariaDB(),
+  dbname = "prueba33_congresovisible2",
+  host = "162.241.62.202",
+  username = "prueba33_cv_user",
+  password = "CongresoVisible2020",
+  port = 3306
+)
+
+
+dbListTables(con)
