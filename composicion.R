6399c017ee7ebb08111443a2616304e69ed6e663
Jaziel Flores <jazzzfm@protonmail.com>
2021-03-03 14:12:22 -0600

    Se suben los de composicion que ya estaban hechos

---
+++ composicion.R
@@ -0,0 +1,34 @@
+library(DBI)
+library(tidyverse)
+library(RMySQL)
+
+con <- dbConnect(
+  drv = RMariaDB::MariaDB(),
+  dbname = "prueba33_congresovisible2",
+  host = "162.241.62.202",
+  username = "prueba33_cv_user",
+  password = "CongresoVisible2020",
+  port = 3306
+)
+
+
+# Composición ------------------------------------------------------------
+# Objetivo: Mostrar la distribución de los partidos y sus miembros dentro del congreso
+
+# Partidos -> Distribución de asientos-Senadores
+tbl(con, "congresistas") %>% filter(activo == 1, es_senador == 1, cuatrienio_id == 1) %>%
+  count(partido_id) %>%
+  left_join(tbl(con,"partidos") %>% select(id,partido = nombre), by = c("partido_id" = "id")) %>% select(-partido_id) %>%
+  show_query()
+
+# Partidos -> Distribución de asientos-Cámara de Representantes
+tbl(con, "congresistas") %>% filter(activo == 1, es_representante_camara == 1, cuatrienio_id == 1) %>% count(partido_id) %>%
+  left_join(tbl(con,"partidos") %>% select(id,partido = nombre), by = c("partido_id" = "id")) %>% select(-partido_id) %>%
+  show_query()
+
+# Partidos -> Edad y sexo
+tbl(con, "congresistas") %>% filter(activo == 1, cuatrienio_id == 1) %>%
+  # left_join(tbl(con,"cuatrienios") %>% select(id,cuatrienio = nombre)) %>%
+  mutate(años = round(DATEDIFF(CURDATE(),fechaNacimiento)/365)) %>%
+  count(partido_id,genero_id,años) %>% left_join(tbl(con,"generos") %>% select(id,genero = nombre), by = c("genero_id" = "id")) %>%
+  left_join(tbl(con,"partidos") %>% select(id,partido = nombre), by = c("partido_id" = "id")) %>% ungroup %>% select(genero,años,partido,n)
--- query_jazzz.R
+++ query_jazzz.R
@@ -13,67 +13,26 @@ con <- dbConnect(
 
 ###################################################################
 #
-# Congreso hoy >> Secciones >> Proyectos de ley
+# Composición >> Secciones >> Congresistas
 #
 ##################################################################
-# Proyectos de ley >> Número de PL en trámite >> Proyectos en Cámara
-tbl(con, "proyecto_leys") %>%
-  filter(activo == 1 && cuatrienio_id == 1) %>%
-  tally() %>%
-  show_query()
 
-SELECT COUNT(*) FROM(
-  SELECT `id`, `titulo`, `numero_camara`
-FROM `proyecto_leys`
-WHERE (`activo` = 1.0 AND `cuatrienio_id` = 1.0))
+# Congresistas -> Pirámide Poblacional de Edad y sexo -> Representantes a la Cámara
+tbl(con, "congresistas") %>% filter(activo == 1, es_representante_camara == 1, cuatrienio_id == 1) %>%
+  mutate(años = round(DATEDIFF(CURDATE(),fechaNacimiento)/365))  %>%
+  count(genero_id,años) %>% left_join(tbl(con,"generos") %>%
+  select(id, genero = nombre), by = c("genero_id" = "id")) %>%
+  ungroup %>% select(genero, años, n) %>%
+  ggplot(pop, aes(x = años, y = n, fill = genero)) +
+  geom_col(data = subset(pop, genero == "Hombres") %>%
+  mutate(`Poblacion por Sexo` = -`Poblacion por Sexo`),
+  width = 0.5, fill = "blue") +
+  geom_col(data = subset(pop, genero == "Mujeres"), width = 0.5, fill = "pink") +
+  coord_flip()
 
-# [1] "id"                             "iniciativa_id"                  "legislatura_id"
-# [4] "cuatrienio_id"                  "tipo_proyecto_id"               "titulo"
-# [7] "numero_camara"                  "numero_senado"                  "tema_proyecto_ley_id"
-# [10] "estado_proyecto_ley_id"         "sinopsis"                       "activo"
-# [13] "usercreated"                    "usermodifed"                    "fecha_radicacion"
-# [16] "tags"                           "old_id"                         "se_acumula_a_id"
-# [19] "camara_id"                      "enlace_a_texto"                 "enlace_a_informe_derecho_justo"
-# [22] "proyecto_ley_estado_id"         "alias"                          "importancia"
-# [25] "destacado"                      "alcance"                        "norma"
-# [28] "created_at"                     "updated_at"                     "proyecto_ley_id"
-
-# Proyectos de ley >> Número de PL en trámite >> Proyectos en Senado
-tbl(con, "proyecto_leys") %>%
-  filter(activo == 1 && cuatrienio_id == 1) %>%
-  mutate(proyecto_ley_id = id) %>%
-  select(proyecto_ley_id, numero_senado) %>%
-  show_query()
-# <SQL>
-#   SELECT `id` AS `proyecto_ley_id`, `numero_senado`
-# FROM `proyecto_leys`
-# WHERE (`activo` = 1.0 AND `cuatrienio_id` = 1.0)
-
-# Proyectos de ley >> Origen de la iniciativa >> Legislativa
-tbl(con, "proyecto_leys") %>%
-  filter(activo == 1 && cuatrienio_id == 1) %>%
-  mutate(proyecto_ley_id = id) %>%
-  select(iniciativa_id, legislatura_id) %>%
-  filter(legislatura_id == 1) %>%
-  show_query()
-# <SQL>
-#   SELECT *
-#   FROM (SELECT `iniciativa_id`, `legislatura_id`
-#         FROM `proyecto_leys`
-#         WHERE (`activo` = 1.0 AND `cuatrienio_id` = 1.0)) `q01`
-# WHERE (`legislatura_id` = 1.0)
-
-# Proyectos de ley >> Origen de la iniciativa >> Gubernamental
-tbl(con, "proyecto_leys") %>%
-  filter(activo == 1 && cuatrienio_id == 1) %>%
-  mutate(proyecto_ley_id = id) %>%
-  select(iniciativa_id, legislatura_id) %>%
-  filter(camara_id == 1) %>%
-  show_query()
-# <SQL>
-#   SELECT *
-#   FROM (SELECT `iniciativa_id`, `legislatura_id`
-#         FROM `proyecto_leys`
-#         WHERE (`activo` = 1.0 AND `cuatrienio_id` = 1.0)) `q01`
-# WHERE (`camara_id` = 1.0)
-
+# Congresistas -> Pirámide Poblacional de Edad y sexo -> Senadores
+tbl(con, "congresistas") %>% filter(activo == 1, es_senador == 1, cuatrienio_id == 1) %>%
+  mutate(años = round(DATEDIFF(CURDATE(),fechaNacimiento)/365))  %>%
+  count(genero_id,años) %>% left_join(tbl(con,"generos") %>%
+                                        select(id, genero = nombre), by = c("genero_id" = "id")) %>%
+  ungroup %>% select(genero, años, n)
