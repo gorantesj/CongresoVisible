0e11074fbf8caa73c58b04e77948e3e6e4d77486
gorantesj <gorantesj@hotmail.com>
2021-03-05 11:31:04 -0600

    Actualizando

--- convertir.R
+++ convertir.R
@@ -18,7 +18,8 @@ crear_bases<- function(relaciones, tablas){
     unique()
   map(tablas_nuevas1,
       ~convertir_grupo1(.x, tablas, relaciones))
-
+  # Tablas nuevas del grupo 2
+  convertir_grupo22(tablas, relaciones)
   #
 }
 
@@ -49,33 +50,24 @@ convertir_grupo1 <- function(tabla_nueva, tablas, relaciones){
             glue::glue("Bases de datos nuevas/{tabla_nueva}.csv"))
 }
 
-convertir_grupo2 <- function(tabla_nueva, tablas, relaciones){
-  # Filtra las relaciones de la tabla nueva
-  relaciones <- relaciones %>%
-    filter(tabla_gp==tabla_nueva,
-           grupo==2)
-  # Separa las relaciones en tablas viejas (usualmente 1)
-  tablas_viejas <- relaciones %>% split(.$num)
-  # Para cada tabla vieja selecciona los campos viejos
-  # Si transformar es igual a 1 le escribe el nombre de campo viejoa con el prefijo
-  # transformar
-  campos_nuevos <- tablas_viejas %>%
+convertir_grupo22 <- function(tablas, relaciones){
+  # Tablas viejas
+  tablas_viejas <- relaciones %>%
+    filter(transformar==2) %>%
+    split(.$num)
+  # Tablas
+  tablas_viejas %>%
     map(~{
-      res <- tablas[[unique(.x$num)]] %>%
-        select(.x$campo)
-      names(res) <- .x$campo_gp
-      names(res)[which(.x$transformar == 1)] <- paste("transformar",
-                                                      .x$campo[which(.x$transformar == 1)],
-                                                      sep = "_")
-      return(res)
-
-    }
-    ) %>%
-    reduce(bind_cols)
-  browser()
-  write_csv(x = campos_nuevos,
-            glue::glue("Bases de datos nuevas/{tabla_nueva}.csv"))
+      tablas_nuevas <- tablas[[unique(.x$num)]]
+      nombre_tabla <- unique(paste0("respaldo_",.x$seccion, .x$tabla))
+      write_csv(x = tablas_nuevas,
+                glue::glue("Bases de datos nuevas/{nombre_tabla}.csv"))
+    })
 }
+
+
+
+
 
 
 # transformación ----------------------------------------------------------
